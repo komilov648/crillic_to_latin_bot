c4435eac7b2680e2585770f5b109eb09ab653532
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-05-16 18:24:22 +0500

    refactor: streamline message handling and improve welcome response

--- main.py
+++ main.py
@@ -11,29 +11,47 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Salom!")
-
-@bot.message_handler(func=lambda message: True)
-def convert_text(message):
-
-    text = message.text
-
-    try:
-        # Latin -> Kirill
-        if text.isascii():
-            result = to_cyrillic(text)
-        else:
-            # Kirill -> Latin
-            result = to_latin(text)
-
-        bot.reply_to(message, result)
-
-    except Exception as e:
-        bot.reply_to(message, f"Xato: {e}")
-
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+     bot.reply_to(message, "Asalamu alaykum, botimizga xush kelibsiz! ")
+ 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+     text = message.text
+     if text.isascii():   
+          bot.reply_to(message, to_cyrillic(text))
+     else:
+          bot.reply_to(message, to_latin(text))
+ 
 bot.infinity_polling()
+
+
+
+
+
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.reply_to(message, "Salom!")
+
+# @bot.message_handler(func=lambda message: True)
+# def convert_text(message):
+
+#     text = message.text
+
+#     try:
+#         # Latin -> Kirill
+#         if text.isascii():
+#             result = to_cyrillic(text)
+#         else:
+#             # Kirill -> Latin
+#             result = to_latin(text)
+
+#         bot.reply_to(message, result)
+
+#     except Exception as e:
+#         bot.reply_to(message, f"Xato: {e}")
+
+# bot.infinity_polling()
 
 
 # import telebot
@@ -42,19 +60,7 @@ bot.infinity_polling()
 # TOKEN = "TOKEN"
 # bot = telebot.TeleBot(TOKEN)
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-#      bot.reply_to(message, "Asalamu alaykum, botimizga xush kelibsiz! ")
- 
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#      text = message.text
-#      if text.isascii():   
-#           bot.reply_to(message, to_cyrillic(text))
-#      else:
-#           bot.reply_to(message, to_latin(text))
- 
-# bot.infinity_polling()
+
 
 
 # s = input()
