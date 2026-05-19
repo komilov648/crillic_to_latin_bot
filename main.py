71a2dc02897bfa6820377ecbba71801b8f3d32d5
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-05-19 17:26:36 +0500

    refactor: improve error handling and streamline message processing

--- main.py
+++ main.py
@@ -1,70 +1,39 @@
 import telebot
 import os
-from dotenv import load_dotenv
+
+from detect_script import detect_script
 from transliterate import to_cyrillic, to_latin
+from dotenv import load_dotenv
 
-
-load_dotenv() 
-
-
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN)
+if not TOKEN:
+    raise ValueError("BOT_TOKEN topilmadi! .env faylni tekshiring.")
+bot = telebot.TeleBot(TOKEN, parse_mode=None) 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-     bot.reply_to(message, "Asalamu alaykum, botimizga xush kelibsiz! ")
- 
-@bot.message_handler(func=lambda m: True)
+	bot.reply_to(
+        message,
+        "Assalom alaykum! Menga lotin yoki kiril matn yuboring.\n"
+        "Lotin → Kiril yoki Kiril → Lotin ga aylantiraman."
+    )
+
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
-     text = message.text
-     if text.isascii():   
-          bot.reply_to(message, to_cyrillic(text))
-     else:
-          bot.reply_to(message, to_latin(text))
- 
+	msg = message.text.strip()
+	script = detect_script(msg)
+
+	if script == 'latin':
+		res = to_cyrillic(msg)
+	else:	
+		res = to_latin(msg)
+	bot.reply_to(message, res)
+	
 bot.infinity_polling()
 
-
-
-
-
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.reply_to(message, "Salom!")
-
-# @bot.message_handler(func=lambda message: True)
-# def convert_text(message):
-
-#     text = message.text
-
-#     try:
-#         # Latin -> Kirill
-#         if text.isascii():
-#             result = to_cyrillic(text)
-#         else:
-#             # Kirill -> Latin
-#             result = to_latin(text)
-
-#         bot.reply_to(message, result)
-
-#     except Exception as e:
-#         bot.reply_to(message, f"Xato: {e}")
-
-# bot.infinity_polling()
-
-
-# import telebot
-# from transliterate import to_cyrillic, to_latin
-
-# TOKEN = "TOKEN"
-# bot = telebot.TeleBot(TOKEN)
-
-
-
-
-# s = input()
-# if s.isascii():
-#     print(to_cyrillic(s))
-# else:
-#     print(to_latin(s))    
+# text = input("Enter text: ")
+# if text.isascii():
+#     print(to_cyrillic(text))
+# else:    print(to_latin(text))   
